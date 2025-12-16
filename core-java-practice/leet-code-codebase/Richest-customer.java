e3dacfcbf1f84330f2e9978b0701f08cdbdcd395
Your Name <youremail@example.com>
2025-12-17 00:09:24 +0530

    Richest Customer Wealth Solved

--- core-java-practice/leet-code-codebase/Richest-customer.java
+++ core-java-practice/leet-code-codebase/Richest-customer.java
@@ -4,7 +4,7 @@
         for (int i = 0; i < acc.length; i++) {
             int sum = 0;//variable to store sum
             for (int j = 0; j < acc[i].length; j++) {
-                sum += acc[i][j];//calculating sum of amount ogmoney of each customer
+                sum += acc[i][j];//calculating sum of amount of money of each customer
             }
             m = Math.max(m, sum);//updating m with max of m and sum to find out the max sum out of all
         }
