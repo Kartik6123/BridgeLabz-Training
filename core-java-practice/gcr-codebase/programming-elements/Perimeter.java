ccab180e2ed8c0d2d9c4679e7c953bc04ccbb5be
Your Name <youremail@example.com>
2025-12-18 14:35:00 +0530

    [Kartik Arora]Perimeter of square  Calculated  Suceesfully

---
+++ core-java-practice/gcr-codebase/programming-elements/Perimeter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Perimeter {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter length of the square ");
+    int length = sc.nextInt();
+    // length of square input
+    int perimeter = 4 * length;
+    // perimeter calculation
+    System.out.println("The perimeter of the square is: " + perimeter);
+    
+   } 
+}
