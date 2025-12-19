ffa542e5365fdd999fd317393a59e756bfa66292
Your Name <youremail@example.com>
2025-12-19 21:17:15 +0530

    [Kartik Arora] Add . Calculated countdown time of rocket

--- core-java-practice/gcr-codebase/control-flow/RocketCountDown.java
+++ core-java-practice/gcr-codebase/control-flow/RocketCountDown.java
@@ -3,15 +3,15 @@ import java.util.*;
 public class RocketCountDown {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter countdown number: ");
-        int counter = sc.nextInt();
-        while (counter >= 1) {
+        System.out.print("Enter the starting count for the rocket launch: ");
+        int launchCounter = sc.nextInt();
+        while (launchCounter >= 1) {
             // Countdown loop
-            System.out.println(counter);
-            counter--;
+            System.out.println(launchCounter);
+            launchCounter--;
         }
-        // Launch message
-        System.out.println(" Launch the rocket");
+          // Launch message
+        System.out.println("Rocket Launched!");
     }
 }
 
