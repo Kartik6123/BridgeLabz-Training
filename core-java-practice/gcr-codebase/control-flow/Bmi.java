6709ed377d30c72eb3d23db81351937aaf4ec0a7
Your Name <youremail@example.com>
2025-12-19 17:00:47 +0530

    [Kartik Arora] Add . Calculated Bmi

---
+++ core-java-practice/gcr-codebase/control-flow/Bmi.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter weight in kg: ");
+        double weight = sc.nextDouble();
+
+        System.out.print("Enter height in cm: ");
+        double heightCm = sc.nextDouble();
+
+        
+        double heightMeter = heightCm / 100;
+        // Convert height from cm to meters
+        
+        double bmi = weight / (heightMeter * heightMeter);
+        // Calculate BMI
+        System.out.println("Your BMI is: " + bmi);
+        if (bmi < 18.5) {
+            //verify underweight
+            System.out.println("Weight Status: Underweight");
+        } else if (bmi >= 18.5 && bmi < 25) {
+            //verify normal weight
+            System.out.println("Weight Status: Normal weight");
+        } else if (bmi >= 25 && bmi < 30) {
+            //verify overweight
+            System.out.println("Weight Status: Overweight");
+        } else {
+            //verify obese
+            System.out.println("Weight Status: Obese");
+        }
+    }
+}
+
