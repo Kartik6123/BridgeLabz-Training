afdcab459edc1d3c2d22535e79f46e84ccf3576e
Your Name <youremail@example.com>
2025-12-19 15:32:32 +0530

    [Kartik Arora] Factors calculated using for loop

--- core-java-practice/gcr-codebase/control-flow/Factors.java
+++ core-java-practice/gcr-codebase/control-flow/Factors.java
@@ -5,14 +5,18 @@ public class Factors {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
+        if (number <= 0) {
+            //negative numbers and zero do not have factors
+            System.out.println("Please enter a positive integer");
+        } else {
             System.out.println("Factors of " + number + " are:");
             for (int i = 1; i < number; i++) {
-                //loop to find factors
+                //check for factors
                 if (number % i == 0) {
-                    //check for factors
                     System.out.println(i);
                 }
             }
         }
     }
+}
 
