d93c7fbc0f7c84852c32db79c94772cb020aae33
Your Name <youremail@example.com>
2025-12-23 16:23:43 +0530

    [Kartik Arora] Add . Metro Fare Calculated Successfully

---
+++ core-java-practice/scenario-based/MetroFare.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class MetroFare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Load initial balance on Metro Smart Card: ₹");
+        int balance = sc.nextInt();
+        while (balance > 0) {
+            System.out.print("Enter distance. Press 0 to exit): ");
+            int distance = sc.nextInt();
+            if (distance == 0) {
+                System.out.println("Journey ended by user.");
+                break;
+            }
+            int fare = 0;
+            // Fare initialization
+            if (distance <= 5) {
+                // Fare calculation
+                fare = 10;
+            } else if (distance <= 15) {
+                fare = 20;
+            } else {
+                fare = 30;
+            }
+            if (balance >= fare) {
+                balance -= fare;
+                System.out.println("Fare deducted: ₹" + fare);
+                System.out.println("Remaining balance: ₹" + balance);
+            } else {
+                System.out.println("Insufficient balance.");
+                break;
+            }
+        }
+    }
+}
