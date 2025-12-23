639febfcba4045cb7d2a7a8373b2d774389d9cb5
Your Name <youremail@example.com>
2025-12-23 15:08:46 +0530

    [Kartik Arora] Add . Code committed successfully

--- CheckDuplicates.java
+++ CheckDuplicates.java
@@ -5,7 +5,7 @@ public class CheckDuplicates {
         System.out.print("Enter array size");
         int size = sc.nextInt();
         int a[]=new int[size];
-        int count=0;
+        int cnt=0;
         System.out.println("Enter array elements");
         for(int i=0;i<size;i++) {
             a[i]=sc.nextInt();
@@ -13,7 +13,7 @@ public class CheckDuplicates {
         for(int i=0;i<size;i++) {
             for(int j=i+1;j<size;j++) {
                  if(a[i]==a[j]) {
-                    count++;
+                    cnt++;
                      System.out.println("Duplicate element found: " + a[i]);
                  }
             }
