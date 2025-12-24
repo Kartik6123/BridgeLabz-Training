91f512d666e71918972444e1ad53de5ee433c017
Your Name <youremail@example.com>
2025-12-24 15:23:55 +0530

    [Kartik Arora] Add .Units converted successfully

--- core-java-practice/scenario-based/ElectionBooth.java
+++ core-java-practice/scenario-based/ElectionBooth.java
@@ -1,28 +1,28 @@
 import java.util.*;
-
-class ElectionBoothManager {
+class ElectionBooth {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         while (true) {
-            System.out.print("Enter age (0 to exit): ");
+            System.out.print("Enter age .Press 0 to exit:");
             int age = sc.nextInt();
-
             if (age == 0) {
+                // Exit condition
                 System.out.println("Election process ended.");
                 break;
             }
-
+            // Age verification
             if (age >= 18) {
-                System.out.print("Cast your vote (1/2/3): ");
+                System.out.print("Cast your vote 1,2,3: ");
                 int vote = sc.nextInt();
-
+                 // Voting process
                 if (vote == 1 || vote == 2 || vote == 3) {
                     System.out.println("Vote recorded successfully.");
                 } else {
+                    // Invalid vote handling
                     System.out.println("Invalid vote.");
                 }
             } else {
+                // Underage handling
                 System.out.println("Not eligible to vote.");
             }
         }
