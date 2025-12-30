a25ab1409d79d4f0fc87a23319dfac60d63a7286
Your Name <youremail@example.com>
2025-12-30 20:28:10 +0530

    [Kartik Arora] Add . Checked wether a string is palindrome or not

---
+++ oops-practice/gcr-codebase/classesandobjects/PalindromeChecker.java
@@ -0,0 +1,31 @@
+package classesandobjects;
+public class PalindromeChecker {
+    String text;
+    PalindromeChecker(String text){
+        this.text = text;
+    }
+    boolean isPalindrome(){
+        String temp=text;
+        String reversed="";
+        for(int i=temp.length()-1;i>=0;i--){
+            reversed=reversed+temp.charAt(i);
+        }
+        if(text.equalsIgnoreCase(reversed)){
+            return true;
+    }
+    else{
+        return false;
+    }
+}
+    void display(){
+        if(isPalindrome()){
+            System.out.println(text + " is a palindrome.");
+        } else {
+            System.out.println(text + " is not a palindrome.");
+        }
+    }
+    public static void main(String[] args) {
+        PalindromeChecker checker = new PalindromeChecker("Madam");
+         checker.display();
+    }
+}
