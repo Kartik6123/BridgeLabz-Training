e017068f8a7d096287604895ec6ffdc9e015e5da
Your Name <youremail@example.com>
2025-12-31 12:39:39 +0530

    [Kartik Arora] Add .Book details displayed by both parameterized and non paramaterized constructor

---
+++ oops-practice/gcr-codebase/contructors/Book.java
@@ -0,0 +1,30 @@
+package contructors;
+public class Book {
+    String title;
+    String author;
+    double price;
+    Book(){
+        this .title = "";
+        this.author = "";
+        this.price = 0.0;
+    }
+    Book(String title, String author, double price){
+        //constructor to initialize book details
+        this.title = title;
+        this.author = author;
+        this.price = price; 
+    }
+    void display(){
+        //method to display book details
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+    public static void main(String[] args) {
+        Book defaultBook = new Book();
+        defaultBook.display();
+
+        Book book = new Book("2 States", "Chetan Bhagat", 500.0);
+        book.display();
+    }
+}
