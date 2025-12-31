5e5f6c3778802cbf8dafc68f394b8cf0818b08c5
Your Name <youremail@example.com>
2025-12-31 15:29:32 +0530

    [Kartik Arora] Add . Course designed

--- oops-practice/gcr-codebase/contructors/Course.java
+++ oops-practice/gcr-codebase/contructors/Course.java
@@ -1,31 +1,51 @@
-package contructors;
+package constructors;
+
+import java.util.Scanner;
 
 public class Course {
- String courseName;
-int duration;
-int fees;
-static String instituteName="GLA";
-   Course(){
-    // Default constructor
-    courseName="";
-    duration=0;
-    fees=0;
-}
-    Course(String courseName,int duration,int fees){
-     // Parameterized constructor
-     this.courseName=courseName;
-     this.duration=duration;
-     this.fees=fees;
+
+    // Instance variables
+    String courseName;
+    int duration;
+    double fee;
+
+    // Class variable
+    static String instituteName;
+
+    // Constructor
+    Course(String courseName, int duration, double fee) {
+        this.courseName = courseName;
+        this.duration = duration;
+        this.fee = fee;
     }
-    void display(){
-     // Display course details
-     System.out.println("Course Name: "+courseName);
-     System.out.println("Duration: "+duration+" months");
-     System.out.println("Fees: "+fees);
-     System.out.println("Institute Name: "+instituteName);
+
+    // Instance method
+    void displayCourseDetails() {
+        System.out.println("Institute: " + instituteName);
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Duration: " + duration + " months");
+        System.out.println("Fee: " + fee);
+        System.out.println();
     }
+
+    // Class method to update institute name
+    static void updateInstituteName(String name) {
+        instituteName = name;
+    }
+
     public static void main(String[] args) {
-     Course c1=new Course();
-     c1.display();
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input for static variable
+        System.out.print("Enter Institute Name: ");
+        String instName = sc.nextLine();
+        Course.updateInstituteName(instName);
+
+        Course c1 = new Course("Java", 6, 25000);
+        Course c2 = new Course("Python", 4, 20000);
+
+        c1.displayCourseDetails();
+        c2.displayCourseDetails();
     }
 }
