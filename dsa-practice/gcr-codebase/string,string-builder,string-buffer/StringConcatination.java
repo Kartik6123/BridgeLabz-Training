55a8d04ad0f32ca7b0824d10f44275a5f2a0e8a2
Your Name <youremail@example.com>
2026-01-16 15:51:18 +0530

    [Kartik Arora] Add . Concatinated Strings using String Buffer

---
+++ dsa-practice/gcr-codebase/string,string-builder,string-buffer/StringConcatination.java
@@ -0,0 +1,14 @@
+class StringConcatenation {
+    public static void main(String[] args) {
+
+        String[] arr = {"Java", " ", "is", " ", "powerful"};
+
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+        }
+        System.out.println(sb.toString());
+    }
+}
+
