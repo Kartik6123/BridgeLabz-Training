867112e4da84be06e5e905ef3d8fdb7d51359b8e
Your Name <youremail@example.com>
2026-02-02 23:38:44 +0530

    [Kartik Arora] Add . Solved annotations and reflection questions

---
+++ collection-practice/gcr-codebase/annotations/RoleManagement/RoleAllowed.java
@@ -0,0 +1,8 @@
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface RoleAllowed {
+    String role();
+}
