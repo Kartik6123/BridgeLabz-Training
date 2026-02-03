686f8d2f092942dc6b7ff162a8bee9ce6b425f07
Your Name <youremail@example.com>
2026-02-03 22:36:36 +0530

    [Kartik Arora] Add . Solved scenario based questions

--- collection-practice/scenario-based/FileBackupScheduler/Main.java
+++ collection-practice/scenario-based/FileBackupScheduler/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
         BackupScheduler scheduler = new BackupScheduler();
-
         try {
             scheduler.addTask(new BackupTask("/system", 10));
             scheduler.addTask(new BackupTask("/documents", 5));
