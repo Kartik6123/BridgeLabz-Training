65e32f2eeae3dde6d8998f144e263d2d607b0a5f
Your Name <youremail@example.com>
2026-02-06 11:18:53 +0530

    [Kartik Arora] Add . Solved regex expression questions

--- collection-practice/gcr-codebase/regex/CapitalizeWord.java
+++ collection-practice/gcr-codebase/regex/CapitalizeWord.java
@@ -2,13 +2,7 @@ import java.util.regex.*;
 public class CapitalizeWord {
     public static  void main(String[] args){
         String str="The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
-
-
         String regex="[A-Z][a-z]*";
-            
-
-        
-        
         Pattern pattern=Pattern.compile(regex);
         Matcher matcher=pattern.matcher(str);
 
